5db68e0492f7ae6fed9b84806997563dc5288c5e
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-10 19:19:40 +0530

    color change

--- app/src/main/java/com/mssinfotech/iampro/co/activities/ProductActivity.java
+++ app/src/main/java/com/mssinfotech/iampro/co/activities/ProductActivity.java
@@ -3,6 +3,8 @@ package com.mssinfotech.iampro.co.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mssinfotech.iampro.co.R;
+
 public class ProductActivity extends AppCompatActivity {
 
     @Override
