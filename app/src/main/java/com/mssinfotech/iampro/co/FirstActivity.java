32a3592a6a5ee9ff2e4bc0275267cd02c5e49863
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-10 18:16:37 +0530

    new file

--- app/src/main/java/com/mssinfotech/iampro/co/FirstActivity.java
+++ app/src/main/java/com/mssinfotech/iampro/co/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.mssinfotech.iampro.co;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class FirstActivity extends Activity {
 
@@ -9,5 +11,13 @@ public class FirstActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(FirstActivity.this, DashboardActivity.class);
+                FirstActivity.this.startActivity(mainIntent);
+                FirstActivity.this.finish();
+            }
+        }, 3000);
     }
 }
