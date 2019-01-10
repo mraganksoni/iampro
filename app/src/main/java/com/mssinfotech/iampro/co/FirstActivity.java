8e8a491a40f6bcdab05393ed13e08ea089d80a14
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-10 18:06:33 +0530

    new file

---
+++ app/src/main/java/com/mssinfotech/iampro/co/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.mssinfotech.iampro.co;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
