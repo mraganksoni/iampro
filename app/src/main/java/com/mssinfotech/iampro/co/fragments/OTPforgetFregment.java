0a2b72312c6172facfe37395fed1c95dc592e0e3
deepMssinfotech <deep.mssinfotech@gmail.com>
2019-01-11 17:06:08 +0530

    new forget otp

---
+++ app/src/main/java/com/mssinfotech/iampro/co/fragments/OTPforgetFregment.java
@@ -0,0 +1,178 @@
+package com.mssinfotech.iampro.co.fragments;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mssinfotech.iampro.co.R;
+import com.mssinfotech.iampro.co.viewmodels.ForgetViewModel;
+
+public class OTPforgetFregment extends Fragment {
+    public String OTP,Email;
+    /* ***********************************
+     * View References
+     *********************************** */
+    private Button btnOTPForget;
+    private TextInputLayout tilOTP;
+    private EditText etOTP;
+
+    private TextInputLayout tilNewPassword;
+    private EditText etNewPassword;
+
+    private TextInputLayout tilConformPassword;
+    private EditText etConformPassword;
+
+
+    /* ***********************************
+     * ViewModels
+     *********************************** */
+    ForgetViewModel forgetViewModel;
+
+    /* ***********************************
+     * SharedPreferences
+     *********************************** */
+    SharedPreferences loginSharedPreferences;
+
+    public OTPforgetFregment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fregment_forget, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ViewModelProvider.AndroidViewModelFactory factory = new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication());
+        forgetViewModel = ViewModelProviders.of(this, factory).get(ForgetViewModel.class);
+        Bundle bundle=getArguments();
+
+        //loginSharedPreferences = Objects.requireNonNull(getContext()).getSharedPreferences(LoginPreferencesConstants.PREF_NAME, Context.MODE_PRIVATE);
+        OTP = bundle.getString("vcode");
+        Email = bundle.getString("email");
+        initViews(view);
+        hookViews();
+        hookIntoViewModel();
+    }
+
+    private void initViews(View view) {
+        btnOTPForget = view.findViewById(R.id.btnOTPForget);
+        tilOTP = view.findViewById(R.id.tilOTP);
+        etOTP = view.findViewById(R.id.etOTP);
+
+        tilNewPassword = view.findViewById(R.id.tilNewPassword);
+        etNewPassword = view.findViewById(R.id.etNewPassword);
+
+        tilConformPassword = view.findViewById(R.id.tilConformPassword);
+        etConformPassword = view.findViewById(R.id.etConformPassword);
+    }
+
+    private void hookViews() {
+        btnOTPForget.setOnClickListener(
+                v -> {
+                    // Check for username
+                    if(!OTP.equals(etOTP.getText().toString())){
+                        Toast.makeText(getContext(), "Validated", Toast.LENGTH_SHORT).show();
+                    }
+                    if (validateNewPassword() && validateConformPassword() && validateOTP())
+                     {
+                        Toast.makeText(getContext(), "Validated", Toast.LENGTH_SHORT).show();
+                        forgetViewModel.startOTPForgetProcess(Email,etNewPassword.getText().toString(),etConformPassword.getText().toString(),etOTP.getText().toString());
+                    }
+                });
+    }
+    private void hookIntoViewModel() {
+        forgetViewModel
+                .getForgetStatus()
+                .observe(
+                        this,
+                        aBoolean -> {
+                            if (aBoolean) {
+                                Toast.makeText(getContext().getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getContext().getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+    }
+
+    private boolean validateNewPassword() {
+        final String data = etNewPassword.getText().toString();
+        String numbers = "(.*[0-9].*)";
+        String upperCaseChars = "(.*[A-Z].*)";
+        String lowerCaseChars = "(.*[a-z].*)";
+        // Check if username is entered
+        if (data == "") {
+            if (!tilNewPassword.isErrorEnabled()) {
+                tilNewPassword.setErrorEnabled(true);
+            }
+            tilNewPassword.setError("Please enter New Password");
+            return false;
+        }else if (data.length() < 4) {
+            if (!tilNewPassword.isErrorEnabled()) {
+                tilNewPassword.setErrorEnabled(true);
+            }
+            tilNewPassword.setError("Password must contant minimum one number and one charecter and minimum length of password  5 ");
+            return false;
+        }else if (!data.matches(numbers )) {
+            if (!tilNewPassword.isErrorEnabled()) {
+                tilNewPassword.setErrorEnabled(true);
+            }
+            tilNewPassword.setError("Password must contant minimum one number and one charecter and minimum length of password  5 ");
+            return false;
+        }else if ((!data.matches(upperCaseChars )) || (!data.matches(lowerCaseChars))) {
+            if (!tilNewPassword.isErrorEnabled()) {
+                tilNewPassword.setErrorEnabled(true);
+            }
+            tilNewPassword.setError("Password must contant minimum one number and one charecter and minimum length of password  5 ");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateConformPassword() {
+        final String data = etConformPassword.getText().toString();
+        final String newdata = etNewPassword.getText().toString();
+        String numbers = "(.*[0-9].*)";
+        String upperCaseChars = "(.*[A-Z].*)";
+        String lowerCaseChars = "(.*[a-z].*)";
+        // Check if username is entered
+        if (data != newdata) {
+            if (!tilConformPassword.isErrorEnabled()) {
+                tilConformPassword.setErrorEnabled(true);
+            }
+            tilConformPassword.setError("New Password And Conform Passwor must be same");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateOTP() {
+        final String data = etOTP.getText().toString();
+
+        // Check if username is entered
+        if (data != OTP) {
+            if (!tilOTP.isErrorEnabled()) {
+                tilOTP.setErrorEnabled(true);
+            }
+            tilOTP.setError("Security code invalid");
+            return false;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/mssinfotech/iampro/co/viewmodels/ForgetViewModel.java
+++ app/src/main/java/com/mssinfotech/iampro/co/viewmodels/ForgetViewModel.java
@@ -7,11 +7,19 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.mssinfotech.iampro.co.fragments.LoginFragment;
+import com.mssinfotech.iampro.co.fragments.OTPforgetFregment;
+import com.mssinfotech.iampro.co.fragments.SignupFragment;
 import com.mssinfotech.iampro.co.utils.LoginPreferencesConstants;
 import com.mssinfotech.iampro.co.utils.VolleyUtil;
 
@@ -23,7 +31,9 @@ public class ForgetViewModel extends AndroidViewModel {
      * Constants
      ****************************************** */
     private static final String TAG = "ForgetViewModel";
-
+    LoginFragment loginFragment;
+    OTPforgetFregment otpforgetFregment;
+    private FragmentActivity myContext;
     /* ******************************************
      * Mutable Live Data references
      ****************************************** */
@@ -64,7 +74,84 @@ public class ForgetViewModel extends AndroidViewModel {
                         });
         VolleyUtil.getInstance(getApplication()).addRequest(request);
     }
+    @SuppressLint("CheckResult")
+    public  void startOTPForgetProcess(String email,String NewPassword,String ConformPassword, String OTP) {
+        final String url =
+                String.format(
+                        "http://www.iampro.co/api/app_service.php?type=resetpassword&email=%s&NewPassword=%s&ConformPassword=%s&process_type=android",
+                        OTP);
+        final JsonObjectRequest request =
+                new JsonObjectRequest(
+                        Request.Method.GET,
+                        url,
+                        null,
+                        (JSONObject response) -> {
+                            try {
+                                final String status = response.getString("status");
+                                if (status.equalsIgnoreCase("error")) {
+                                    forgetStatus.setValue(false);
+                                } else if (status.equalsIgnoreCase("success")) {
+                                    redirectLogin();
+                                    //getFullUserDetails(userId);
+                                    forgetStatus.setValue(true);
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        },
+                        error -> {
+                            Log.e(TAG, "startLoginProcess: a", error.getCause());
+                        });
+        VolleyUtil.getInstance(getApplication()).addRequest(request);
+    }
+    private void redirectLogin(){
+        if (loginFragment == null) {
+            loginFragment = new LoginFragment();
+            Transition modeTransition = TransitionInflater.from(myContext).inflateTransition(android.R.transition.move);
+            Transition fadeTransition = TransitionInflater.from(myContext).inflateTransition(android.R.transition.fade);
+            loginFragment.setSharedElementEnterTransition(modeTransition);
+            loginFragment.setSharedElementReturnTransition(modeTransition);
+            loginFragment.setEnterTransition(fadeTransition);
+            loginFragment.setExitTransition(fadeTransition);
+        }
+        /*
+        Bundle args = new Bundle();
+        args.putString("name", "mragank");
+        loginFragment.setArguments(args);
+        */
+
+        FragmentManager fragmentManager = myContext.getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(android.R.id.content, loginFragment, "LOGIN")
+                .setTransition(android.R.transition.move)
+                .addToBackStack("LOGIN")
+                .commit();
+
+    }
     private void redirectToOTPForget(String email,String vcode){
+        if (otpforgetFregment == null) {
+            otpforgetFregment = new OTPforgetFregment();
+           Transition modeTransition = TransitionInflater.from(myContext).inflateTransition(android.R.transition.move);
+            Transition fadeTransition = TransitionInflater.from(myContext).inflateTransition(android.R.transition.fade);
+            otpforgetFregment.setSharedElementEnterTransition(modeTransition);
+            otpforgetFregment.setSharedElementReturnTransition(modeTransition);
+            otpforgetFregment.setEnterTransition(fadeTransition);
+            otpforgetFregment.setExitTransition(fadeTransition);
+
+        }
+
+        Bundle args = new Bundle();
+        args.putString("email", email);
+        args.putString("vcode", vcode);
+        loginFragment.setArguments(args);
+
+
+        FragmentManager fragmentManager = myContext.getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(android.R.id.content, otpforgetFregment, "OTPForget")
+                .setTransition(android.R.transition.move)
+                .addToBackStack("OTPForget")
+                .commit();
 
     }
     /*
