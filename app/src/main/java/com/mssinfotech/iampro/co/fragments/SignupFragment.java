8dbeb5ed906028b3e1ea48d0c8765dd90afefe37
mssinfotech <mragankshekhar@ms2fun.com>
2019-01-11 12:55:48 +0530

    add sms read permission

--- app/src/main/java/com/mssinfotech/iampro/co/fragments/SignupFragment.java
+++ app/src/main/java/com/mssinfotech/iampro/co/fragments/SignupFragment.java
@@ -8,6 +8,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.mssinfotech.iampro.co.R;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mssinfotech.iampro.co.R;
+import com.mssinfotech.iampro.co.events.NewLoginEvent;
+import com.mssinfotech.iampro.co.utils.LoginPreferencesConstants;
+import com.mssinfotech.iampro.co.viewmodels.LoginViewModel;
+import com.mssinfotech.iampro.co.viewmodels.RegisterViewModel;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
---
+++ app/src/main/java/com/mssinfotech/iampro/co/viewmodels/RegisterViewModel.java
@@ -0,0 +1,8 @@
+package com.mssinfotech.iampro.co.viewmodels;
+
+/**
+ * Created by mssinfotech on 11/01/19.
+ */
+
+public class RegisterViewModel {
+}
